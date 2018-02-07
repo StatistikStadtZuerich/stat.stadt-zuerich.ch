65bfd16cff800b8de449cc83a4c3fa8a9153f12c
bergi <bergi@axolotlfarm.org>
2018-02-07 11:59:08 +0100

    updated shapes api

--- api/BEV018-shape.sparql.es6
+++ api/BEV018-shape.sparql.es6
@@ -1,18 +1,53 @@
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX qb: <http://purl.org/linked-data/cube#>
 PREFIX sh: <http://www.w3.org/ns/shacl#>
 
 CONSTRUCT {
-  <http://stat.stadt-zuerich.ch/api/dataset/BEV018/shape> a sh:NodeShape .
-  <http://stat.stadt-zuerich.ch/api/dataset/BEV018/shape> sh:property ?property .
-  ?property ?p ?o .
-  ?o rdfs:label ?label .
+  <http://stat.stadt-zuerich.ch/api/dataset/BEV018/shape> a sh:NodeShape ;
+    sh:property ?b_property .
+
+  ?b_property sh:in ?propertyValue ;
+    rdfs:seeAlso ?dimension .
+
+  ?propertyValue rdfs:label ?label .
+
+  ?dimension rdfs:label ?dimensionLabel ;
+    skos:notation ?dimensionNotation .
+
+  # all the list fun
+  ?b_property sh:path ?b_list .
+
+  ?b_list rdf:first qb:observation ;
+    rdf:rest ?b_nil .
+
+  ?b_nil rdf:first ?dimension ;
+    rdf:rest rdf:nil .
 } WHERE {
   GRAPH <https://linked.opendata.swiss/graph/zh/statistics> {
-    <http://stat.stadt-zuerich.ch/view/BEV018/shape> sh:property ?property .
-    ?property ?p ?o .
+    {
+      <http://stat.stadt-zuerich.ch/view/BEV018/shape> sh:property ?b_property .
 
-    OPTIONAL {
-      ?o rdfs:label ?label .
+      ?b_property sh:in ?propertyValue .
+
+      OPTIONAL {
+        ?propertyValue rdfs:label ?label .
+      }
+
+      FILTER(isIRI(?propertyValue))
+    } UNION {
+      <http://stat.stadt-zuerich.ch/view/BEV018/shape> sh:property ?b_property .
+
+      ?b_property sh:path ?b_list .
+
+      ?b_list rdf:first qb:observation ;
+        rdf:rest ?b_nil .
+
+      ?b_nil rdf:first ?dimension ;
+        rdf:rest rdf:nil .
+
+      ?dimension rdfs:label ?dimensionLabel ;
+        skos:notation ?dimensionNotation .
     }
   }
 }
--- build/build-views-jsonld-context.js
+++ build/build-views-jsonld-context.js
@@ -2,24 +2,47 @@ const fs = require('fs')
 const path = require('path')
 const u = require('./utils')
 
-function buildViewsJsonldContext (properties) {
-  const context = JSON.parse(fs.readFileSync(path.join(__dirname, 'support/slice.context.jsonld')).toString())
+function buildPropertiesContext (properties, stubFilename, outputFilename, short) {
+  const context = JSON.parse(fs.readFileSync(stubFilename).toString())
 
   properties.forEach((property) => {
     const key = (property.iri.startsWith('http://ld.stadt-zuerich.ch/statistics/measure/') ? 'm' : 'd') + property.notation
 
-    context['@context'][key] = {
-      '@id': property.iri
-    }
+    if (short) {
+      context['@context'][key] = property.iri
+    } else {
+      context['@context'][key] = {
+        '@id': property.iri
+      }
 
-    if (u.isNotZeit(property.iri)) {
-      context['@context'][key]['@type'] = property.datatype || '@id'
+      if (u.isNotZeit(property.iri)) {
+        context['@context'][key]['@type'] = property.datatype || '@id'
+      }
     }
   })
 
-  fs.writeFileSync(path.join(__dirname, '../api/slice.context.jsonld'), JSON.stringify(context, null, '  '))
+  fs.writeFileSync(outputFilename, JSON.stringify(context, null, '  '))
 
   return Promise.resolve(context)
+}
+
+function buildViewsSliceJsonldContext (properties) {
+  return buildPropertiesContext(properties,
+    path.join(__dirname, 'support/slice.context.jsonld'),
+    path.join(__dirname, '../api/slice.context.jsonld'))
+}
+
+function buildViewsShapeJsonldContext (properties) {
+  return buildPropertiesContext(properties,
+    path.join(__dirname, 'support/shape.context.jsonld'),
+    path.join(__dirname, '../api/shape.context.jsonld'), true)
+}
+
+function buildViewsJsonldContext (properties) {
+  return Promise.all([
+    buildViewsSliceJsonldContext(properties),
+    buildViewsShapeJsonldContext(properties)
+  ])
 }
 
 module.exports = buildViewsJsonldContext
