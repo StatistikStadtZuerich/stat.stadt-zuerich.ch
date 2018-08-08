24c7b200767b45637f4d93b9bd0236815210c58a
Michael Rauch <git.mra@miranet.ch>
2018-08-08 15:24:35 +0200

    ssz-integ: reduced shape api (dataset whitelisting) and cache-control

--- api-config.js
+++ api-config.js
@@ -8,20 +8,55 @@ module.exports = {
 
   outDir: 'api',
 
+  // *** WHITELISTING ***
   viewFilter: view => {
-    const blacklist = [
-      'BES-RAUM-ZEIT-0',
-      'GEB-RAUM-ZEIT-NAF-NAM-SEX'
+    const notation = view.notation.value;
+
+    const whitelist = [
+      // landing page
+      'WRT-RAUM-ZEIT-ARA-OBJ',
+      'GBF-RAUM-ZEIT-ARA',
+      'GBD-RAUM-ZEIT-ARA',
+
+      // verwandte statistiken
+      'AVA-RAUM-ZEIT-GGH-SEX',
+      'AVA-RAUM-ZEIT-HEL-SEX',
+      'AVA-RAUM-ZEIT-HEL-PSA',
+      'AVA-RAUM-ZEIT-ELK-HEL-PSA',
+      'GES-RAUM-ZEIT-HEL-SEX',
+      'GES-RAUM-ZEIT-ALT',
+      'GES-RAUM-ZEIT-HEL',
+      'GES-RAUM-ZEIT-ALT-TOU',
+      'GES-RAUM-ZEIT-SEX-TOU',
+      'SNB-RAUM-ZEIT-HEL',
+      'SWB-RAUM-ZEIT-HEO',
+
+      // for simple shakedown
+      'BEW-RAUM-ZEIT-HEL',
+
+      // for testing observations with NaN 
+      'ZUS-RAUM-ZEIT-BTA-VSA',
+      'WHG-RAUM-ZEIT-ZIM'
     ];
 
-    const notation = view.notation.value;    
-    for (i = 0; i < blacklist.length; i++) {
-      if (notation.startsWith(blacklist[i])) {
-        console.log(`blacklisted & skipped: ${notation}`);
-        return false;
-      }
-    }
-
-    return true;
+    return whitelist.includes(notation);
   }
+
+  // *** BLACKLISTING ***
+  // viewFilter: view => {
+  //   const blacklist = [
+  //     'BES-RAUM-ZEIT-0',
+  //     'GEB-RAUM-ZEIT-NAF-NAM-SEX'
+  //   ];
+
+  //   const notation = view.notation.value;    
+  //   for (i = 0; i < blacklist.length; i++) {
+  //     if (notation.startsWith(blacklist[i])) {
+  //       console.log(`blacklisted & skipped: ${notation}`);
+  //       return false;
+  //     }
+  //   }
+
+  //   return true;
+  // }
 }
