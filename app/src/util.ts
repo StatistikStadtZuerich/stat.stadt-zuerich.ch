ee7202c4b730f054f404cbd2801094e2f931fbb0
Michael Rauch <git.mra@miranet.ch>
2018-08-14 12:02:42 +0200

    cleanup obsolete app

--- app/src/util.ts
+++
@@ -1,2 +0,0 @@
-export const toArray = <T extends {}>(x: T | T[] | void): T[] =>
-  x === undefined ? [] : Array.isArray(x) ? x : [x];
--- app/typings/catalog.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "catalog" {
-  export const markdown: any;
-  export const ReactSpecimen: any;
-  export const HintSpecimen: any;
-}
